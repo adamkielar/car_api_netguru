f33542ca746ac090877e6b462d4bc5401064bb58
Adam Kielar <adam@adamkielar.pl>
2020-08-24 14:57:28 +0200

    Started django project app

--- app/app/settings.py
+++ app/app/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve(strict=True).parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'i+hs7n8&m(h+g%ke$^n#hu^uovqtm0atort(5^nxcwzl2o+lx1'
+SECRET_KEY = 'f6y=g+#r#czncwjjhhi-_f(h5#g@5w9id5&6_j^#94!lpslhgn'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
