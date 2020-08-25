cc80fe006a33ca4297f64a1e6c81d1b3333e6345
Adam Kielar <adam@adamkielar.pl>
2020-08-25 09:44:14 +0200

    Added Car model

--- app/core/models.py
+++ app/core/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
-# Create your models here.
+
+class Car(models.Model):
+    car_make = models.CharField(max_length=255)
+    car_model = models.CharField(max_length=255)
+    rating = models.PositiveSmallIntegerField(blank=True, null=True)
+
+    class Meta:
+        unique_together = ['car_make', 'car_model']
+
+    def __str__(self):
+        return f'{self.car_make} {self.car_model}'
